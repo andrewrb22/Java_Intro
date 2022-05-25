b5e5ccbe3fb782c41fe9a9edfe2bbfc7e61daf66
Andrew Rodriguez <andrewrb22@gmail.com>
2022-05-24 23:15:56 -0400

    Added comments to the code

--- HW1.java
+++ HW1.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
-public class Driver {
+public class HW1 {
 public static void main(String[] args) {
+   //Variables
     String fullname ;
     String empnumber ;
     double hours;
@@ -11,6 +12,8 @@ public static void main(String[] args) {
 
     Scanner myScan;
     myScan = new Scanner (System.in);
+
+    //Scanning 
 
 System.out.print("\nEnter the Employee's full name: ");
 fullname = myScan.nextLine();
@@ -27,10 +30,13 @@ System.out.print("Enter the reagular hours worked: ");
 hours = (double) myScan.nextFloat();
 ;
 
+//Calculating total, deduction tax and net pay
+
 total = (double)rate * hours;
 tax = (double)total * 0.06;
 net = (double)total - (double)tax;
 
+//Printing paycheck
 
 System.out.print("\nThank you!\n");
 System.out.print("---------------------------------------------\n");
