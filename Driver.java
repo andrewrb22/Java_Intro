3adb187b4cece1d6e0f5686b6b8b5e378dd9cadb
Andrew Rodriguez <andrewrb22@gmail.com>
2022-05-24 23:10:18 -0400

    Finish running test, everything is working

--- Driver.java
+++ Driver.java
@@ -3,7 +3,7 @@ public class Driver {
 public static void main(String[] args) {
     String fullname ;
     String empnumber ;
-    int hours;
+    double hours;
     double rate;
     double total;  
     double tax; 
@@ -12,35 +12,37 @@ public static void main(String[] args) {
     Scanner myScan;
     myScan = new Scanner (System.in);
 
-System.out.print("Enter the Employee's full name: ");
+System.out.print("\nEnter the Employee's full name: ");
 fullname = myScan.nextLine();
 
 System.out.print("Enter the Employee's ID number: ");
 empnumber = myScan.nextLine();
 
 System.out.print("Enter pay rate per hour: ");
-rate = (int) myScan.nextFloat();
+rate = (double) myScan.nextFloat();
 
 
 ;
 System.out.print("Enter the reagular hours worked: ");
-hours = (int) myScan.nextFloat();
+hours = (double) myScan.nextFloat();
 ;
 
 total = (double)rate * hours;
 tax = (double)total * 0.06;
 net = (double)total - (double)tax;
 
+
 System.out.print("\nThank you!\n");
+System.out.print("---------------------------------------------\n");
 System.out.print("\nHere is your paycheck!\n\n");
 System.out.print("---------------------------------------------\n");
 System.out.println("Employee's Name: " + fullname);
-System.out.println("\nEmployee's Number: " + empnumber);
-System.out.println("\nHourly rate of pay: " + rate);
-System.out.println("\nHours Worked: " + hours);
+System.out.println("Employee's Number: " + empnumber);
+System.out.println("Hourly rate of pay: " + rate);
+System.out.println("Hours Worked: " + hours);
 System.out.println("\nTotal Gross Pay: $" + total);
-System.out.println("\nDeductions\nTax(%6): $" + tax);
-System.out.println("\nNet Pay: $" + net + " Dollars");
+System.out.println("Deductions\nTax(%6): $" + tax);
+System.out.println("Net Pay: $" + net + " Dollars");
 
 System.out.print("-------------------------------------------");
 myScan.close();
