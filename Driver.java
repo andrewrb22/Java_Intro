f0e1bf695bb2c9c9a4f8a28482161589ac4db3b5
Andrew Rodriguez <andrewrb22@gmail.com>
2022-05-24 22:41:41 -0400

    Testing

--- Driver.java
+++ Driver.java
@@ -1,33 +1,38 @@
 import java.util.Scanner;
 public class Driver {
 public static void main(String[] args) {
-// System.out.println("Hello World!");
-//float x = 2.5f  , d;
-//System.out.print("Enter your number: ");
-Scanner myScan;
-myScan = new Scanner (System.in);
-//x = myScan.nextDouble();
-/*long  x = 5;
-int d;
-d = 2 * (int)x;
-System.out.println(x + " * 2 = " + d);
-*/
-//System.out.printf("[%-10.2f]\n", 7.0/2);
-//System.out.println(  7/(double)2  );
-    // int x , y , z;
-// x = y = z = 7;
-//System.out.println(2%7%5);
-//System.out.println(2 +  "" + 3 + "Something" );
-String fullname ;
-String empnumber ;
-int hours;
-int rate;
-double total;
-total = (int)rate * (int)hours;
-double tax;
-tax = (double)total * 0.06;
-double net;
-net = (double)total - (double)tax;
+    String fullname ;
+    String empnumber ;
+    int hours;
+    double rate;
+    double total;  
+    double tax; 
+    double net;
+
+    Scanner myScan;
+    myScan = new Scanner (System.in);
+
+// import java.util.*;  
+// public class ScannerNextFloatExample1 {    
+//     public static void main(String args[]){       
+//         int amount;  
+//         double balance;  
+//         //Insert amount and balance from console  
+//         Scanner input = new Scanner (System.in);  
+//         System.out.print("Enter the amount: ");  
+//         amount = input.nextInt();  
+//         System.out.print("Enter the Total Balance: ");  
+//         balance = input.nextFloat();  
+//         //Reduce amount+fee from balance  
+//         balance = balance-(amount + 0.50);  
+//         //Print new balance  
+//         System.out.print("New Balance is: "+balance);  
+//       input.close();  
+//     }    
+
+
+
+
 
 System.out.print("Enter the Employee's full name: ");
 fullname = myScan.nextLine();
@@ -36,14 +41,17 @@ System.out.print("Enter the Employee's ID number: ");
 empnumber = myScan.nextLine();
 
 System.out.print("Enter pay rate per hour: ");
-rate = (int) myScan.nextDouble();
-myScan = new Scanner(System.in)
+rate = (int) myScan.nextFloat();
+
 
 ;
 System.out.print("Enter the reagular hours worked: ");
 hours = myScan.nextInt();
-myScan = new Scanner(System.in)
 ;
+
+total = (double)rate * hours;
+tax = (double)total * 0.06;
+net = (double)total - (double)tax;
 
 System.out.print("Thank you\n!");
 System.out.print("\nHere is your paycheck!\n\n\n\n");
@@ -57,5 +65,6 @@ System.out.println("\n\nDeductions\nTax(%6): $" + tax);
 System.out.println("\n\nNet Pay: $" + net + " Dollars");
 
 System.out.print("-------------------------------------------");
+myScan.close();
 }
 }
