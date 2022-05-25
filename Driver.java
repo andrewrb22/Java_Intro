ebb2c2eaa55a74a26108d49bda1ac79ddc552d21
Andrew Rodriguez <andrewrb22@gmail.com>
2022-05-24 22:51:11 -0400

    Updated rate scanner to next.float

--- Driver.java
+++ Driver.java
@@ -12,28 +12,6 @@ public static void main(String[] args) {
     Scanner myScan;
     myScan = new Scanner (System.in);
 
-// import java.util.*;  
-// public class ScannerNextFloatExample1 {    
-//     public static void main(String args[]){       
-//         int amount;  
-//         double balance;  
-//         //Insert amount and balance from console  
-//         Scanner input = new Scanner (System.in);  
-//         System.out.print("Enter the amount: ");  
-//         amount = input.nextInt();  
-//         System.out.print("Enter the Total Balance: ");  
-//         balance = input.nextFloat();  
-//         //Reduce amount+fee from balance  
-//         balance = balance-(amount + 0.50);  
-//         //Print new balance  
-//         System.out.print("New Balance is: "+balance);  
-//       input.close();  
-//     }    
-
-
-
-
-
 System.out.print("Enter the Employee's full name: ");
 fullname = myScan.nextLine();
 
@@ -46,23 +24,23 @@ rate = (int) myScan.nextFloat();
 
 ;
 System.out.print("Enter the reagular hours worked: ");
-hours = myScan.nextInt();
+hours = (int) myScan.nextFloat();
 ;
 
 total = (double)rate * hours;
 tax = (double)total * 0.06;
 net = (double)total - (double)tax;
 
-System.out.print("Thank you\n!");
-System.out.print("\nHere is your paycheck!\n\n\n\n");
-System.out.print("---------------------------------------------\n\n\n");
+System.out.print("\nThank you!\n");
+System.out.print("\nHere is your paycheck!\n\n");
+System.out.print("---------------------------------------------\n");
 System.out.println("Employee's Name: " + fullname);
 System.out.println("\nEmployee's Number: " + empnumber);
 System.out.println("\nHourly rate of pay: " + rate);
 System.out.println("\nHours Worked: " + hours);
-System.out.println("\n\nTotal Gross Pay: $" + total);
-System.out.println("\n\nDeductions\nTax(%6): $" + tax);
-System.out.println("\n\nNet Pay: $" + net + " Dollars");
+System.out.println("\nTotal Gross Pay: $" + total);
+System.out.println("\nDeductions\nTax(%6): $" + tax);
+System.out.println("\nNet Pay: $" + net + " Dollars");
 
 System.out.print("-------------------------------------------");
 myScan.close();
